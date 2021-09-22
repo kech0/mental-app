c5ddb9dd5c1ebc8d74f6cf0e0f05b743f0e152ce
Louisiana-Alela <louisiana.alela@student.moringaschool.com>
2021-09-23 00:43:09 +0300

    Modified

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,17 @@ const doctors= [
     He has had tremendous experience of expertise gained at top hospitals. <br>
     He also has 5 years of experience in Psychology field, especially with those dealing with
     substance abuse.`, 'Addiction', 'doctr1.jpg'),
-    new doctor('Dr. Emily VanCamp',`Dr. Bryce is amongst the best addiction psychologist in the part of the world. <br>
-    He has had tremendous experience of expertise gained at top hospitals. <br>
-    He also has 5 years of experience in Psychology field, especially with those dealing with
-    substance abuse.`, 'PTSD', 'doctr2.jpg'),
-    new doctor('Dr. Jack Bryce',`Dr. Bryce is amongst the best addiction psychologist in the part of the world. <br>
-    He has had tremendous experience of expertise gained at top hospitals. <br>
-    He also has 5 years of experience in Psychology field, especially with those dealing with
-    substance abuse.`, 'Financial stress', 'doc3.jpeg'),
-    new doctor('Dr.Renee Wilson',`Dr. Bryce is amongst the best addiction psychologist in the part of the world. <br>
-    He has had tremendous experience of expertise gained at top hospitals. <br>
-    He also has 5 years of experience in Psychology field, especially with those dealing with
+    new doctor('Dr. Emily VanCamp',` Dr. VanCamp is one of the best financial stress psychologist that we have. <br>
+    She has great experience of expertise gained at top hospitals. <br>
+    She also has 3 years of experience in Psychology field, especially with those dealing with
+    financial constraints.`, 'Financial stress', 'doct2.jpg'),
+    new doctor('Dr. Danish Mayal',`Dr. Mayal is one of the top PTSD psychologist we could ever ask for. <br>
+    He has experience of expertise gained at top known hospitals. <br>
+    He also has 6 years of experience in Psychology field, especially with those dealing with
+    PTSD.`, 'PTSD', 'doc3.jpeg'),
+    new doctor('Dr.Renee Wilson',`Dr. Wilson is among the best addiction psychologist in this part of the world. <br>
+    She is well conversant with mental health with experience of gained at most hospitals. <br>
+    She has 8 years of experience in Psychology field, especially with those dealing with
     substance abuse.`, 'Addiction', 'doc4.jpg')
 
 ]
