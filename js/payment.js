1d938c26b1323b882c619880d0fa528bc87a54be
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-23 14:55:48 +0300

    This commit adds the payment images

--- js/payment.js
+++ js/payment.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     console.log(summaryDiv)
     summaryDiv.find(".dr-name").html(doctor.name);
     summaryDiv.find(".specialization").html(doctor.specialization);
-    summaryDiv.find("img").attr("src", "/images/" + doctor.image);
+    summaryDiv.find(".image-doc").attr("src", "/images/" + doctor.image);
     summaryDiv.find(".date").html(schedule.date);
     summaryDiv.find(".time").html(schedule.time);
 })
