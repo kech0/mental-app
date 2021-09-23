6056e26d22bbbc4c17ff0f057d2c701857e22300
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-23 12:34:43 +0300

    This commit adds the schedule for the booking form

--- js/order.js
+++ js/order.js
@@ -1,25 +1,24 @@
-import {doctors} from "./scripts"
 let doctor;
-console.log(doctors)
+
 $(document).ready(function () {
-    doctor =JSON.parse(localStorage.getItem('selectedDoctor'));
+    doctor = JSON.parse(localStorage.getItem('selectedDoctor'));
     console.log(doctor)
-    const cardDiv=$("#dr-card")
+    const cardDiv = $("#dr-card")
     console.log(cardDiv)
     cardDiv.find("h5.card-title").html(doctor.name);
     cardDiv.find(".card-text").html(doctor.description);
-    cardDiv.find("img").attr("src","/images/"+ doctor.image);
-    $("#booking-form").on("submit",function(evt){
+    cardDiv.find("img").attr("src", "/images/" + doctor.image);
+    $("#booking-form").on("submit", function (evt) {
         evt.preventDefault()
-        if(!this.checkValidity()){
+        if (!this.checkValidity()) {
             $(this).addClass("was-validated");
         }
 
-        const schedule={
-            date:this.date.value,
-            time:this.hour.value
+        const schedule = {
+            date: this.date.value,
+            time: this.hour.value
         }
-        localStorage.setItem("schedule",JSON.stringify(schedule))
-        window.location="/payment.html"
+        localStorage.setItem("schedule", JSON.stringify(schedule))
+        window.location = "/payment.html"
     })
 })
