b7a79bed41d0c1a320aad38e5d86382d728b6aa5
Martin909 <>
2021-09-23 13:27:07 +0300

    resets the form

--- js/find.js
+++ js/find.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
     $('#contactForm').on('submit', function(evt){
         evt.preventDefault();
         alert("Thank you! " + this.name.value + " For you feedback");
+        
     });
 });
+function resetForm() {
+    document.getElementById("contactForm").reset();
+  }
